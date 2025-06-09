9af087b8bf08d19bf4446ee8f10754ad7776e614
daemosity <evanescentink@gmail.com>
2025-06-08 20:55:46 -0500

    Adding middleware, clicktrackers, error Logger

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,41 @@
 import express from "express";
+import { config } from "./config.js";
 const app = express();
-const port = 8080;
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// })
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+const PORT = 8080;
+const rootPath = "/app";
+const staticPath = "./src/app";
+app.use(middlewareLogResponses);
+app.use(rootPath, middlewareMetricsInc, express.static(staticPath));
+app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerRequestHitCount);
+app.get("/reset", handlerRequestHitCountReset);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/app/`);
 });
+async function handlerReadiness(_, res) {
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send("OK");
+}
+;
+async function handlerRequestHitCount(_, res) {
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send(`Hits: ${config.fileserverHits}`);
+}
+async function handlerRequestHitCountReset(_, res) {
+    config.fileserverHits = 0;
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send("Reset Confirmed");
+}
+function middlewareLogResponses(req, res, next) {
+    res.on("finish", () => {
+        const statusCode = res.statusCode;
+        if (statusCode >= 400) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+        }
+    });
+    next();
+}
+function middlewareMetricsInc(req, res, next) {
+    config.fileserverHits += 1;
+    next();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,50 @@
-import express from "express";
+import express, {NextFunction, Request, Response} from "express";
+import { config } from "./config.js";
 
 const app = express();
-const port = 8080;
+const PORT = 8080;
+const rootPath = "/app";
+const staticPath = "./src/app";
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// })
+app.use(middlewareLogResponses);
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+app.use(rootPath, middlewareMetricsInc, express.static(staticPath));
+
+app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerRequestHitCount);
+app.get("/reset", handlerRequestHitCountReset);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}/app/`);
 });
+
+async function handlerReadiness(_: Request, res: Response): Promise<void> {
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send("OK");
+};
+
+async function handlerRequestHitCount(_: Request, res: Response) {
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send(`Hits: ${config.fileserverHits}`)
+}
+
+async function handlerRequestHitCountReset(_: Request, res: Response) {
+    config.fileserverHits = 0;
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send("Reset Confirmed");
+}
+
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        const statusCode = res.statusCode;
+        if (statusCode >= 400) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`)
+        }
+    });
+    next()
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+    config.fileserverHits += 1;
+    next()
+}
